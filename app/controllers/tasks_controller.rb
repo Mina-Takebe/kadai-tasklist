9a1a334cd3f1515c400cad790f7341600b4e1900
m.takebe <m.bamboo0825@gmail.com>
2020-08-16 09:35:37 +0000

    add one to many relationship

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
+  before_action :correct_user, only: [:show, :edit, :destroy]
 	
   def index
-    @tasks = Task.order(id: :asc).page(params[:page]).per(10)
+     @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(10)
   end
 
   def show
-  	@task = Task.find(params[:id])
+  	@task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -14,35 +15,34 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'Taskを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
+      flash.now[:danger] = 'Taskの投稿に失敗しました。'
       render :new
     end
   end
 
   def edit
-  	@task = Task.find(params[:id])
+  	@task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'Task の更新に失敗しました'
       render :edit
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -54,5 +54,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
